e2e8ca418ba04661e07a855a3ea9f83da08dc599
ibyeonggyu <ibyeonggyu410@gmail.com>
2026-05-18 19:17:37 +0900

    feat: [VEDA-19] problem2 구현

---
+++ srcs/week01/problem2/ibyeonggyu.cpp
@@ -0,0 +1,29 @@
+/**
+ * 문제: 프로그래머스 [PCCE 기출문제] 1번 / 문자 출력
+ * 링크: https://school.programmers.co.kr/learn/courses/30/lessons/340207
+ * 접근법: std::cout을 사용해서 출력 (std::endl은 flush까지 수행하므로 이스케이프 문자인 '\n'으로 출력)
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int apple = 0, banana = 0, pear = 0;
+	int cnt = 0;
+
+	cin >> apple >> banana >> pear;
+	cnt = pear / 4;
+
+	if (cnt > (banana / 2))
+	{
+		cnt = banana / 2;
+		cout << 7 * cnt;
+	}
+	else
+		cout << 7 * cnt;
+	return 0;
+}
