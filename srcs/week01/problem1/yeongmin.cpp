989ff756875bb0ceff0badd118a897c8a8645f01
lym0217 <peter2326@naver.com>
2026-05-19 09:29:58 +0900

    feat: [VEDA-23] 1번 문제 풀이 완료

---
+++ srcs/week01/problem1/yeongmin.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, y, day = 0;
+
+	cin >> x >> y;
+
+	for(int i=1; i<x; i++)
+	{
+		switch(i)
+		{
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+				day+=31;
+				break;
+			case 2:
+				day+=28;
+				break;
+			default:
+				day+=30;
+				break;
+		}
+	}
+	day+=y;
+	cout << 366-day <<"\n";
+	return 0;
+}
