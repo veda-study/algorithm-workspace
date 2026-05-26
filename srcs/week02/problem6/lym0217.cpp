0d9434202c9c65cd12fe59541f8132ddd7562aa7
lym0217 <peter2326@naver.com>
2026-05-26 19:28:18 +0900

    feat: [VEDA-46] 6번 문제 풀이 완료

--- srcs/week02/problem6/lym0217.cpp
+++ srcs/week02/problem6/lym0217.cpp
@@ -1,14 +1,17 @@
 /**
  * 문제: change
  * 링크: http://59.23.132.199/30stair/change/change.php?pname=change
- * 접근법: 
+ * 접근법: 그냥 단순 계산
  */
 
 #include <iostream>
 
+using namespace std;
 int main(void)
 {
-    
+    int n;
+    cin >> n;
+    cout << (1000-n)/100 << " " << ((1000-n)%100)/50 << " " << (((1000-n)%100)%50)/10 << "\n";
 
     return 0;
 }
