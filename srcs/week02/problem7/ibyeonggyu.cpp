19e08f1e5d9464e80783cec1426e6af0e708ec81
ibyeonggyu <ibyeonggyu410@gmail.com>
2026-05-26 11:12:33 +0900

    feat: [VEDA-57] 기초문제 7번 풀이 완료

--- srcs/week02/problem7/ibyeonggyu.cpp
+++ srcs/week02/problem7/ibyeonggyu.cpp
@@ -1,14 +1,27 @@
 /**
- * 문제: leap_year
+ * 문제: 7번 문제풀이
  * 링크: http://59.23.132.199/30stair/leap_year/leap_year.php?pname=leap_year
- * 접근법: 
+ * 접근법: 두개의 조건을 구현하였습니다.
  */
 
 #include <iostream>
 
+using namespace std;
+
 int main(void)
 {
-    
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
-    return 0;
+	int input;
+	cin >> input;
+
+	if ((input % 4 == 0 && input % 100 != 0) || (input % 400 == 0))
+	{
+		cout << "YES" << endl;
+	}
+	else cout << "NO" << endl;
+
+
+	return 0;
 }
