ff6852f098aa6097658c3ddde8c7c0accba52c3d
je0sa <jjhdokkj@gmail.com>
2026-05-26 14:01:17 +0900

    feat: [VEDA-66] 문제 6번 풀이 완료

--- srcs/week02/problem6/je0sa.cpp
+++ srcs/week02/problem6/je0sa.cpp
@@ -1,14 +1,23 @@
 /**
- * 문제: change
- * 링크: http://59.23.132.199/30stair/change/change.php?pname=change
- * 접근법: 
+ * 문제: 6번
+ * 링크: 
+ * 접근법: 나머지 연산을 이용하였습니다.
  */
 
 #include <iostream>
 
+using namespace std;
+
 int main(void)
 {
-    
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+    int money, m;
+
+    cin >> money;
+
+    m = 1000 - money;
+    cout << m/100 << " " << (m%100)/50 << " " << ((m%100)%50)/10;
     return 0;
 }
