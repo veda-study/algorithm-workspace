0698c7b7babf1a295f5f62766c3cb785941d9308
lym0217 <peter2326@naver.com>
2026-05-27 09:24:15 +0900

    feat: [VEDA-47] 7번 문제 풀이 완료

--- srcs/week02/problem7/lym0217.cpp
+++ srcs/week02/problem7/lym0217.cpp
@@ -1,14 +1,19 @@
 /**
  * 문제: leap_year
  * 링크: http://59.23.132.199/30stair/leap_year/leap_year.php?pname=leap_year
- * 접근법: 
- */
+ * 접근법: 조건문 이용
+**/
 
 #include <iostream>
 
+using namespace std;
 int main(void)
 {
-    
+    int n;
+
+    cin >> n;
+    if((n%400 == 0) || ((n%4 == 0) && (n%100 != 0))) cout << "YES\n";
+    else cout << "NO\n";
 
     return 0;
 }
