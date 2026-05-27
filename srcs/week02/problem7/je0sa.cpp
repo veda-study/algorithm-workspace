facce3c322e656eb66bd3a648b3dfd894464f17f
je0sa <jjhdokkj@gmail.com>
2026-05-27 10:18:58 +0900

    [Week 02]: [VEDA-67] 문제 7번 풀이 완료" - je0sa (#39)
    ## 📌 관련 Jira 이슈
    * 연동 이슈: [VEDA-67]
    
    ## 📝 접근 방식
    * 조건문과 논리연산자(&&,||)를 통해 해결 했습니다. 
    
    ## ⚠️ 고민했던 점 및 질문
    * 
    
    [VEDA-67]:
    https://veda-study.atlassian.net/browse/VEDA-67?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- srcs/week02/problem7/je0sa.cpp
+++ srcs/week02/problem7/je0sa.cpp
@@ -1,14 +1,29 @@
 /**
- * 문제: leap_year
- * 링크: http://59.23.132.199/30stair/leap_year/leap_year.php?pname=leap_year
+ * 문제: 7번 문제
+ * 링크: http://59.23.132.199/30stair/change/change.php?pname=change
  * 접근법: 
  */
 
 #include <iostream>
 
+using namespace std;
+
 int main(void)
 {
-    
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+    int year;
+
+    cin >> year;
+
+    if(((year%4) == 0 && (year%100)!= 0) || (year%400 == 0))
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
     return 0;
 }
