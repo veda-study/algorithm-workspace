9ff93d878909de1bdfca9d296bbbc9e448974fcb
je0sa <jjhdokkj@gmail.com>
2026-05-28 18:19:01 +0900

    feat: [VEDA-68] 8번 문제 풀이 완료

--- srcs/week02/problem8/je0sa.cpp
+++ srcs/week02/problem8/je0sa.cpp
@@ -1,14 +1,25 @@
 /**
  * 문제: beat
  * 링크: http://59.23.132.199/30stair/beat/beat.php?pname=beat
- * 접근법: 
+ * 접근법: 2차방정식을 통해 해결했습니다.
  */
 
 #include <iostream>
 
+using namespace std;
 int main(void)
 {
-    
+    int s,d,a,b;
+  
+    cin >> s >> d;
+    // 2차 방정식 활용  
 
+    a = (s+d)/2;
+    b = s - a;
+
+    if((s+d)%2 == 1) cout <<"impossible";
+
+    if(b< 0) cout <<"impossible";
+    else if( a > 0 || b > 0) cout << a << " " << b << "\n";
     return 0;
 }
