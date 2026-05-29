2e5cf64e3b00bd0a52978a53b4de01a1addfa5e8
mangjun <myoungjun55@naver.com>
2026-05-29 21:16:56 +0900

    [Week02]: [VEDA-80] 10번 문제 풀이 완료

--- srcs/week02/problem10/mangjun.cpp
+++ srcs/week02/problem10/mangjun.cpp
@@ -1,14 +1,33 @@
 /**
  * 문제: selfdup
  * 링크: http://59.23.132.199/30stair/selfdup/selfdup.php?pname=selfdup
- * 접근법: 
+ * 접근법: 그냥 반복문
  */
 
 #include <iostream>
 
+using namespace std;
+
+void solve();
+
 int main(void)
 {
-    
+    solve();
 
     return 0;
+}
+
+void solve()
+{
+    int d = 10;
+
+    for (int i = 1; i <= 10000; i++) {
+        if ((i * i) % d == i) {
+            cout << i << "\n";
+        }
+
+        if (i == d) {
+            d = d * 10;
+        }
+    }
 }
