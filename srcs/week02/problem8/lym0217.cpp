981c6a607475bfd62b5d14a42151733ec939801b
Yeongmin_Lee <65957439+lym0217@users.noreply.github.com>
2026-05-29 16:37:56 +0900

    [Week02]: [VEDA-48] 8번 문제 풀이 완료 - lym0217 (#44)
    ## 📌 관련 Jira 이슈
    * 연동 이슈: [VEDA-48]
    
    ## 📝 접근 방식
    * 2차 방정식으로 접근
    
    ## ⚠️ 고민했던 점 및 질문
    * 없습니다~
    
    [VEDA-48]:
    https://veda-study.atlassian.net/browse/VEDA-48?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- srcs/week02/problem8/lym0217.cpp
+++ srcs/week02/problem8/lym0217.cpp
@@ -1,14 +1,21 @@
 /**
  * 문제: beat
  * 링크: http://59.23.132.199/30stair/beat/beat.php?pname=beat
- * 접근법: 
+ * 접근법: 방정식 사용
  */
 
 #include <iostream>
 
+using namespace std;
 int main(void)
 {
-    
+    int a, b;
 
+    cin >> a >> b;
+    if((a+b)%2) cout << "impossible\n";
+    else{
+    	if((a-b)/2 < 0) cout << "impossible\n";
+    	else cout << (a+b)/2 << " " << (a-b)/2 << "\n";
+    }
     return 0;
 }
