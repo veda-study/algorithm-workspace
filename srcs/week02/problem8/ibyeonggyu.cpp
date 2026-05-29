d62645e26a45bf1664edbd6d17a86e859c40aec3
ibyeonggyu <ibyeonggyu410@gmail.com>
2026-05-29 16:37:21 +0900

    [Week 02]: [VEDA-58] 8번 기초문제 풀이 완료 - ibyeonggyu (#42)
    ## 📌 관련 Jira 이슈
    * 연동 이슈: [VEDA-58]
    
    ## 📝 접근 방식
    * 하라는대로 했습니다.
    
    ## ⚠️ 고민했던 점 및 질문
    * 모두모두 화이팅
    
    [VEDA-58]:
    https://veda-study.atlassian.net/browse/VEDA-58?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- srcs/week02/problem8/ibyeonggyu.cpp
+++ srcs/week02/problem8/ibyeonggyu.cpp
@@ -1,14 +1,29 @@
 /**
  * 문제: beat
  * 링크: http://59.23.132.199/30stair/beat/beat.php?pname=beat
- * 접근법: 
+ * 접근법: 합과 차를 이용해서 큰 수와 작은 수를 찾았습니다.
  */
 
 #include <iostream>
 
+using namespace std;
+
 int main(void)
 {
-    
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
-    return 0;
+	int s, d; // s: 합, d: 차
+	int a, b; // a: 큰 수, b: 작은 수
+
+	cin >> s >> d;
+
+	a = (s + d) / 2;
+	b = s - a;
+
+	if ((s + d) % 2 == 1) cout << "impossible" << endl;
+	else if (a < 0 || b < 0) cout << "impossible" << endl;
+	else cout << a << " " << b << endl;
+
+	return 0;
 }
