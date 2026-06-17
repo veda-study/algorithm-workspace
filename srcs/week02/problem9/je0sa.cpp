4f7762f9596b85df6679be31d6022c14bf232ca6
je0sa <jjhdokkj@gmail.com>
2026-06-17 16:43:08 +0900

    [Week 02]: [VEDA-69] 9번 문제 풀이 완료 - je0sa (#48)
    ## 📌 관련 Jira 이슈
    * 연동 이슈: [VEDA-69]
    
    ## 📝 접근 방식
    * 몫 연산을 통해 해결했습니다.
    
    ## ⚠️ 고민했던 점 및 질문
    * 시간복잡도 고려해서 실행속도가 더 빠른 코드를 알고싶습니다.
    
    [VEDA-69]:
    https://veda-study.atlassian.net/browse/VEDA-69?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- srcs/week02/problem9/je0sa.cpp
+++ srcs/week02/problem9/je0sa.cpp
@@ -1,14 +1,30 @@
 /**
  * 문제: adigit
  * 링크: http://59.23.132.199/30stair/adigit/adigit.php?pname=adigit
- * 접근법: 
+ * 접근법: 몫연산을 통해 해결했습니다.
  */
 
 #include <iostream>
+using namespace std;
 
 int main(void)
-{
-    
+{ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+    int arr[7] = {0,};
+    // 한자리 정수, 두자리 정수, 세자리 정수
+    int num1 = 0,num2 = 0,num3 = 0; 
+
+    for(int i = 0; i<7; i++)
+    {
+        cin >> arr[i];
+
+        if(arr[i]/100 > 0) num3 += arr[i];
+        else if(arr[i]/10 > 0) num2 += arr[i];
+        else num1 += arr[i];
+        
+    }
+    cout << num1 << " " << num2 << " " << num3 << "\n";
     return 0;
 }
