642001efd7a9ea0ab8f4efa20ed4d5ad532e93b8
ibyeonggyu <ibyeonggyu410@gmail.com>
2026-06-29 13:32:56 +0900

    [Week 02]: [VEDA-60] 10번 기초문제 풀이 완료 - ibyeonggyu (#46)
    ## 📌 관련 Jira 이슈
    * 연동 이슈: [VEDA-60]
    
    ## 📝 접근 방식
    * 자릿수(size)를 어떻게 구현할까 고민하다가 문자열로 받아 ary.size로 
    받아와서 해결했습니다.
    * 문제를 해결하는 수식을 만들었습니다.
    
    ## ⚠️ 고민했던 점 및 질문
    * 업슴니다..
    
    [VEDA-60]:
    https://veda-study.atlassian.net/browse/VEDA-60?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- srcs/week02/problem10/ibyeonggyu.cpp
+++ srcs/week02/problem10/ibyeonggyu.cpp
@@ -1,14 +1,29 @@
 /**
  * 문제: selfdup
  * 링크: http://59.23.132.199/30stair/selfdup/selfdup.php?pname=selfdup
- * 접근법: 
+ * 접근법: 10, 100, 1000, 10000 자릿 수를 기준으로 문제를 구분해서 풀었습니다.
  */
 
 #include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
 
 int main(void)
 {
-    
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
-    return 0;
+	for (int i = 1; i <= 10000;i++) {
+		int num = i * i;
+		string ary = to_string(i);
+		int size = pow(10, ary.size());
+
+		if ((num - i) % size == 0) cout << i << endl;
+		// 5*5 = 25 에서 -5를 빼면 20인데 이게 끝자리가 0으로 나오면 해당함
+		// 25*25 = 625 인데 -25를 하면 600 이고 뒤에 25는 두 자리이므로 
+		// 뒤에가 00으로 끝나기에 %100 할 때 나눠지므로 조건에 해당함
+	}
+
+	return 0;
 }
