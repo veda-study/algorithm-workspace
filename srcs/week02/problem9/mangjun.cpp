a080566f4e314023120fec28565505aa4fb09212
Kim Myoung Jun <myoungjun55@naver.com>
2026-06-29 13:34:11 +0900

    [Week02]: [VEDA-79] 문제 9번 풀이 완료 - mangjun (#50)
    ## 📌 관련 Jira 이슈
    * 연동 이슈: [VEDA-79]
    
    ## 📝 접근 방식
    * 큰 수부터 비교
    
    ## ⚠️ 고민했던 점 및 질문
    * -
    
    [VEDA-79]:
    https://veda-study.atlassian.net/browse/VEDA-79?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- srcs/week02/problem9/mangjun.cpp
+++ srcs/week02/problem9/mangjun.cpp
@@ -1,14 +1,46 @@
 /**
  * 문제: adigit
  * 링크: http://59.23.132.199/30stair/adigit/adigit.php?pname=adigit
- * 접근법: 
+ * 접근법: 큰 수부터 비교
  */
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve(const vector<int>& vc);
 
 int main(void)
 {
-    
+    int n;
+    vector<int> vc;
+
+    for (int i = 0; i < 7; i++) {
+        cin >> n;
+        vc.push_back(n);
+    }
+
+    solve(vc);
 
     return 0;
+}
+
+void solve(const vector<int>& vc)
+{
+    int oneSum = 0, twoSum = 0, threeSum = 0;
+
+    for (const int n : vc) {
+        if (n >= 100) {
+            threeSum += n;
+        }
+        else if (n >= 10) {
+            twoSum += n;
+        }
+        else {
+            oneSum += n;
+        }
+    }
+
+    cout << oneSum << " " << twoSum << " " << threeSum << "\n";
 }
