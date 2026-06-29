f245bde7d19b29a06f10a6dca51c3c234097886a
Kim Myoung Jun <myoungjun55@naver.com>
2026-06-29 13:34:43 +0900

    [Week02]: [VEDA-77] 문제 7번 풀이 완료 - mangjun (#52)
    ## 📌 관련 Jira 이슈
    * 연동 이슈: [VEDA-77]
    
    ## 📝 접근 방식
    * -
    
    ## ⚠️ 고민했던 점 및 질문
    * -
    
    [VEDA-77]:
    https://veda-study.atlassian.net/browse/VEDA-77?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- srcs/week02/problem7/mangjun.cpp
+++ srcs/week02/problem7/mangjun.cpp
@@ -1,14 +1,32 @@
 /**
  * 문제: leap_year
  * 링크: http://59.23.132.199/30stair/leap_year/leap_year.php?pname=leap_year
- * 접근법: 
+ * 접근법: if문
  */
 
 #include <iostream>
 
+using namespace std;
+
+void solve(int year);
+
 int main(void)
 {
-    
+    int year;
+
+    cin >> year;
+
+    solve(year);
 
     return 0;
+}
+
+void solve(int year)
+{
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        cout << "YES" << "\n";
+    }
+    else {
+        cout << "NO" << "\n";
+    }
 }
